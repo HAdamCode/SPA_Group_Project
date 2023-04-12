f2096396ce1df75d551ef3657985630264f4f0cd
Mike5280 <74518500+Mike5280@users.noreply.github.com>
2023-04-11 19:13:06 -0600

    removing duplicate code

--- lab7express/src/util.js
+++
@@ -1,8 +0,0 @@
-import { MongoClient } from 'mongodb';
-
-export async function connectMongodb() {
-    const client = new MongoClient('mongodb://localhost:27017');
-    await client.connect();
-
-    return client.db('local');
-}
